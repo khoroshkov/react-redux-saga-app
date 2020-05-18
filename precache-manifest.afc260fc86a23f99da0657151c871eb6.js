376f6cac18b4652ce59b0eaa177369e591e0ea01
Nick_Khoroshkov <nickolaykhoroshkov@gmail.com>
2020-05-18 11:55:15 +0300

    Updates

---
+++ precache-manifest.afc260fc86a23f99da0657151c871eb6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe33674d489a6d3afcf01ec9563f0f93",
+    "url": "/react-redux-saga-app/index.html"
+  },
+  {
+    "revision": "08dc44807bc8217350c7",
+    "url": "/react-redux-saga-app/static/css/main.0236f0de.chunk.css"
+  },
+  {
+    "revision": "37100bc075c4f89a1a91",
+    "url": "/react-redux-saga-app/static/js/2.17d68e2e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-redux-saga-app/static/js/2.17d68e2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08dc44807bc8217350c7",
+    "url": "/react-redux-saga-app/static/js/main.0069dc66.chunk.js"
+  },
+  {
+    "revision": "d1d662165680a90fdc3f",
+    "url": "/react-redux-saga-app/static/js/runtime-main.a7217cce.js"
+  }
+]);
